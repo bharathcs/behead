01592b8c38ac6e315797b72632578addd608de77
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2022-03-21 21:39:49 +0800

    Add tests and refactor

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func behead(r io.Reader, wr io.Writer, numberOfLinesToSkip int64) error {
 	lineCount := int64(0)
 	done := make(chan interface{}, 1)
 	if wr != os.Stdout {
-		go report(&lineCount, reportingDuration, done)
+		go report(&lineCount, reportingDuration, done, os.Stdout)
 	}
 
 	for sc.Scan() {
@@ -55,7 +55,7 @@ func behead(r io.Reader, wr io.Writer, numberOfLinesToSkip int64) error {
 
 		_, err := wr.Write([]byte(sc.Text() + "\n"))
 		if err != nil {
-			exitIfError(fmt.Errorf("unable to write to output anymore: %w", err))
+			return fmt.Errorf("unable to write to output anymore: %w", err)
 		}
 		atomic.AddInt64(&lineCount, 1)
 	}
@@ -67,13 +67,13 @@ func behead(r io.Reader, wr io.Writer, numberOfLinesToSkip int64) error {
 	return nil
 }
 
-func report(rowCountAddr *int64, reportingDuration time.Duration, stop <-chan interface{}) {
+func report(lineCountAddr *int64, reportingDuration time.Duration, stop <-chan interface{}, wr io.Writer) {
 	var (
 		start              time.Time
 		now                time.Time
 		lastTime           time.Time
-		lastRowCount       int64
-		rowCount           int64
+		lastLineCount      int64
+		lineCount          int64
 		timeElapsed        time.Duration
 		overallTimeElapsed time.Duration
 		rate               float64
@@ -84,29 +84,30 @@ func report(rowCountAddr *int64, reportingDuration time.Duration, stop <-chan in
 
 	select {
 	case _ = <-stop:
-		printLine(overallTimeElapsed, rate, overallRate, rowCount, "\n")
+		printLine(wr, overallTimeElapsed, rate, overallRate, lineCount, "\n")
 		break
 	case now = <-time.NewTicker(reportingDuration).C:
 		now = time.Now()
-		rowCount = atomic.LoadInt64(rowCountAddr)
+		lineCount = atomic.LoadInt64(lineCountAddr)
 
-		rate = float64(rowCount-lastRowCount) / timeElapsed.Seconds()
+		rate = float64(lineCount-lastLineCount) / timeElapsed.Seconds()
 		timeElapsed = now.Sub(lastTime)
 
-		overallRate = float64(rowCount) / now.Sub(start).Seconds()
+		overallRate = float64(lineCount) / now.Sub(start).Seconds()
 		overallTimeElapsed = now.Sub(start)
 
-		printLine(overallTimeElapsed, rate, overallRate, rowCount, "\r")
-		lastRowCount = rowCount
+		printLine(wr, overallTimeElapsed, rate, overallRate, lineCount, "\r")
+		lastLineCount = lineCount
 		lastTime = now
 	}
 }
 
-func printLine(overallTimeElapsed time.Duration, rate float64, overallRate float64, rowCount int64, suffix string) {
-	fmt.Printf(
+func printLine(wr io.Writer, overallTimeElapsed time.Duration, rate float64, overallRate float64, lineCount int64, suffix string) {
+	fmt.Fprintf(
+		wr,
 		"%v have elapsed. %0.2f lines/sec (last %v), %0.2f lines/sec (overall), %d total rows     %s",
 		time.Duration(overallTimeElapsed-(overallTimeElapsed%time.Second)),
-		rate, reportingDuration, overallRate, rowCount, suffix,
+		rate, reportingDuration, overallRate, lineCount, suffix,
 	)
 }
 
---
+++ main_test.go
@@ -0,0 +1,335 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"reflect"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+)
+
+func Test_behead_happyPath(t *testing.T) {
+	type args struct {
+		r                   io.Reader
+		numberOfLinesToSkip int64
+	}
+	tests := []struct {
+		name   string
+		args   args
+		wantWr string
+	}{
+		{
+			name: "no input",
+			args: args{
+				r:                   strings.NewReader(""),
+				numberOfLinesToSkip: 100,
+			},
+			wantWr: "",
+		},
+		{
+			name: "overshoot lines to skip",
+			args: args{
+				r:                   strings.NewReader(""),
+				numberOfLinesToSkip: 10,
+			},
+			wantWr: "",
+		},
+		{
+			name: "simple test",
+			args: args{
+				r:                   strings.NewReader("foo\nbar\nbaz\n"),
+				numberOfLinesToSkip: 1,
+			},
+			wantWr: "bar\nbaz\n",
+		},
+		{
+			name: "will add terminating newline",
+			args: args{
+				r:                   strings.NewReader("foo\nbar\nbaz"),
+				numberOfLinesToSkip: 2,
+			},
+			wantWr: "baz\n",
+		},
+		{
+			name: "will add terminating newline",
+			args: args{
+				r:                   strings.NewReader("foo\nbar\nbaz"),
+				numberOfLinesToSkip: 1,
+			},
+			wantWr: "bar\nbaz\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			wr := &bytes.Buffer{}
+			err := behead(tt.args.r, wr, tt.args.numberOfLinesToSkip)
+			if err != nil {
+				t.Errorf("behead() error = %v, wantErr %v", err, false)
+				return
+			}
+			if gotWr := wr.String(); gotWr != tt.wantWr {
+				t.Errorf("behead() gotWr = %v, want %v", unquote(gotWr, t), unquote(tt.wantWr, t))
+			}
+		})
+	}
+}
+
+type badReader struct{}
+type badWriter struct{}
+
+func (b badWriter) Write(p []byte) (n int, err error) {
+	return 0, fmt.Errorf("oops")
+}
+
+func (b badReader) Read(p []byte) (n int, err error) {
+	return 0, fmt.Errorf("oops")
+}
+
+func Test_behead_errors(t *testing.T) {
+	type args struct {
+		r                   io.Reader
+		w                   io.Writer
+		numberOfLinesToSkip int64
+	}
+	tests := []struct {
+		name            string
+		args            args
+		errStringPrefix string
+	}{
+		{
+			name: "bad reader",
+			args: args{
+				r:                   badReader{},
+				w:                   &bytes.Buffer{},
+				numberOfLinesToSkip: 100,
+			},
+			errStringPrefix: "did not end reading cleanly:",
+		},
+		{
+			name: "bad writer",
+			args: args{
+				r:                   strings.NewReader("foo\n"),
+				w:                   badWriter{},
+				numberOfLinesToSkip: 0,
+			},
+			errStringPrefix: "unable to write to output anymore:",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := behead(tt.args.r, tt.args.w, tt.args.numberOfLinesToSkip)
+			if err == nil {
+				t.Errorf("behead() error = %v, wantErr %q", err, tt.errStringPrefix)
+				return
+			}
+			if !strings.Contains(err.Error(), tt.errStringPrefix) {
+				t.Errorf("behead() gotWr = %v, want %v", err, tt.errStringPrefix)
+				return
+			}
+		})
+	}
+}
+
+func Test_getReader(t *testing.T) {
+
+	tempDir := path.Join(os.TempDir(), "getReader")
+	os.RemoveAll(tempDir)
+	os.Mkdir(tempDir, 0755)
+
+	emptyFilepath := path.Join(tempDir, "emptyFile")
+	normalFilepath := path.Join(tempDir, "normalFile")
+	nonExistentFilepath := path.Join(tempDir, "nonExistentFile")
+
+	normalFileContents := ""
+	for i := 0; i < 100; i++ {
+		normalFileContents += fmt.Sprint(i) + "\n"
+	}
+
+	if _, err := os.Create(emptyFilepath); err != nil {
+		t.Fatal("unable to run test: temp files could not be created", err)
+		return
+	}
+
+	if f, err := os.OpenFile(normalFilepath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0755); err == nil {
+		_, err = f.WriteString(normalFileContents)
+	} else if err != nil && f.Close() != nil {
+		t.Fatal("unable to run test: temp files could not be created", err)
+		return
+	}
+
+	tests := []struct {
+		name     string
+		filepath string
+		wantR    string
+		wantErr  bool
+	}{
+		{
+			name:     "empty file",
+			filepath: emptyFilepath,
+			wantR:    "",
+			wantErr:  false,
+		},
+		{
+			name:     "non empty file",
+			filepath: normalFilepath,
+			wantR:    normalFileContents,
+			wantErr:  false,
+		},
+		{
+			name:     "non existent file",
+			filepath: nonExistentFilepath,
+			wantR:    "",
+			wantErr:  true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotR, err := getReader(tt.filepath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getReader() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if err == nil {
+				if contents, _ := ioutil.ReadAll(gotR); string(contents) != tt.wantR {
+					t.Errorf("getReader() gotR = %q, want %q", unquote(string(contents), t), tt.wantR)
+				}
+			}
+		})
+	}
+
+	t.Run("empty filepath", func(t *testing.T) {
+		gotR, err := getReader("")
+		if err != nil {
+			t.Errorf("getReader() err = %v", err)
+		}
+		if gotR != os.Stdin {
+			t.Errorf("getReader() gotR = %v, want %v", gotR, os.Stdin)
+		}
+	})
+}
+
+func Test_getWriter(t *testing.T) {
+
+	tempDir := path.Join(os.TempDir(), "getWriter")
+	os.RemoveAll(tempDir)
+	os.Mkdir(tempDir, 0755)
+
+	nonExistentFilepath := path.Join(tempDir, "notAFileYet")
+	normalFilepath := path.Join(tempDir, "normalFile")
+
+	normalFileContents := ""
+	for i := 0; i < 100; i++ {
+		normalFileContents += fmt.Sprint(i) + "\n"
+	}
+
+	if _, err := os.Stat(nonExistentFilepath); os.IsExist(err) {
+		t.Fatal("unable to run test: temp files exists where it shouldn't", err)
+	}
+	if f, err := os.OpenFile(normalFilepath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0755); err == nil {
+		_, err = f.WriteString(normalFileContents)
+	} else if err != nil {
+		t.Fatal("unable to run test: temp files could not be created", err)
+	}
+
+	newContent := "foo\n"
+
+	t.Run("test non existent file", func(t *testing.T) {
+		gotW, err := getWriter(nonExistentFilepath)
+		gotW.Write([]byte(newContent))
+
+		var res []byte
+
+		if err != nil {
+			t.Errorf("getWriter() error = %v, wantErr %v", err, false)
+			return
+		}
+		if file, err := os.Open(nonExistentFilepath); err != nil {
+			t.Errorf("getWriter() did not create an openable file: %v", err)
+		} else if res, err = ioutil.ReadAll(file); err != nil {
+			t.Errorf("getWriter() created a file that ioutil gave an error for: %v", err)
+		}
+
+		if string(res) != ""+newContent {
+			t.Errorf("getWriter()'s wr saved %q instead of %q", unquote(string(res), t), unquote(""+newContent, t))
+		}
+	})
+
+	t.Run("test existent file", func(t *testing.T) {
+		gotW, err := getWriter(normalFilepath)
+		gotW.Write([]byte(newContent))
+
+		var res []byte
+
+		if err != nil {
+			t.Errorf("getWriter() error = %v, wantErr %v", err, false)
+			return
+		}
+		if file, err := os.Open(normalFilepath); err != nil {
+			t.Errorf("getWriter() did not create an openable file: %v", err)
+		} else if res, err = ioutil.ReadAll(file); err != nil {
+			t.Errorf("getWriter() created a file that ioutil gave an error for: %v", err)
+		}
+
+		if string(res) != normalFileContents+newContent {
+			t.Errorf("getWriter()'s wr saved %q instead of %q", unquote(string(res), t), unquote(normalFileContents+newContent, t))
+		}
+	})
+
+	t.Run("empty filepath", func(t *testing.T) {
+		gotW, err := getWriter("")
+		if err != nil {
+			t.Errorf("getWriter() error = %v, wantErr %v", err, false)
+			return
+		}
+		if !reflect.DeepEqual(gotW, os.Stdout) {
+			t.Errorf("getWriter() gotR = %v, want %v", gotW, os.Stdout)
+		}
+	})
+}
+
+func Test_report(t *testing.T) {
+	lineCount := int64(94)
+
+	name := "all in one"
+	lineCountAddr := &lineCount
+	customReportingDuration := time.Duration(10) * time.Millisecond
+	stop := make(chan interface{}, 1)
+
+	r, w, _ := os.Pipe()
+
+	t.Run(name, func(t *testing.T) {
+		go report(lineCountAddr, customReportingDuration, stop, w)
+
+		time.Sleep(time.Duration(104) * time.Millisecond)
+		close(stop)
+		time.Sleep(time.Duration(10) * time.Millisecond)
+		w.Close()
+
+		res, err := ioutil.ReadAll(r)
+		if err != nil {
+			t.Fatalf("failed to read from mocked Stdin %v", err)
+			return
+		}
+
+		resEscaped := unquote(string(res), t)
+
+		if n := strings.Count(string(res), "\r"); n > 1 {
+			t.Errorf("report() wanted returns, instead got %q", resEscaped)
+			return
+		}
+	})
+}
+
+func unquote(res string, t *testing.T) string {
+	resEscaped, err := strconv.Unquote(`"` + string(res) + `"`)
+	if err != nil {
+		t.Fatalf("failed to escape from from mocked Stdin %v", err)
+	}
+	return resEscaped
+}
